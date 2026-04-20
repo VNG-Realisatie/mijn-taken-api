88ea104e58a5f2ce6667b1ecc61b21dfeffc3256
Rutger ter Borg <rutger.terborg@artibit.com>
2026-04-20 07:04:12 +0000

    Concept Uitgangspunten opgesteld

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -75,7 +75,7 @@ const config: Config = {
           type: 'docSidebar',
           sidebarId: 'tutorialSidebar',
           position: 'left',
-          label: 'Tutorial',
+          label: 'Docs',
         },
         {
           href: 'https://github.com/vng-realisatie/mijn-taken-api',
